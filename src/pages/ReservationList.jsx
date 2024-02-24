53a724fbb073e37aea9b81bdc0d02bbeae04961e
UttkarshSharma23 <uttkarsh.sharma2019@gmail.com>
2024-02-25 02:22:30 +0530

    .

--- src/pages/ReservationList.jsx
+++ src/pages/ReservationList.jsx
@@ -17,7 +17,7 @@ const ReservationList = () => {
   const getReservationList = async () => {
     try {
       const response = await fetch(
-        `https://home-rentals-backend.onrender.com/users/${userId}/reservations`,
+        `http://localhost:3001/users/${userId}/reservations`,
         {
           method: "GET",
         }
--- src/pages/TripList.jsx
+++ src/pages/TripList.jsx
@@ -17,7 +17,7 @@ const TripList = () => {
   const getTripList = async () => {
     try {
       const response = await fetch(
-        `https://home-rentals-backend.onrender.com/users/${userId}/trips`,
+        `http://localhost:3001/users/${userId}/trips`,
         {
           method: "GET",
         }
